b4f836333fd1fc0f260006aefb6518f7b42d9064
edaeren <edaeren2001@outlook.com>
2024-03-13 11:54:50 +0300

    Initial Commit

---
+++ CertificateAppV2/CertificateAppV2App.swift
@@ -0,0 +1,17 @@
+//
+//  CertificateAppV2App.swift
+//  CertificateAppV2
+//
+//  Created by Eda Eren on 13.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct CertificateAppV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
