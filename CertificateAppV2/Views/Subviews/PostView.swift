2e1a9632eef891c979232ef8bf7cd7f29e4f3722
edaeren <edaeren2001@outlook.com>
2024-03-15 00:48:47 +0300

    added scrollviews to the certificate page(just for now i used a forEach, will change later.) + added some certificate picture samples

---
+++ CertificateAppV2/Views/Subviews/PostView.swift
@@ -0,0 +1,18 @@
+//
+//  PostView.swift
+//  CertificateAppV2
+//
+//  Created by Eda Eren on 14.03.2024.
+//
+
+import SwiftUI
+
+struct PostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostView()
+}
